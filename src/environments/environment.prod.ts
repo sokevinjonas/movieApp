c52ddb74941843264fb9bfad6f08d0b17e2eeedf
jonasdevpro <kjonasdevpro@gmail.com>
2024-03-20 12:23:31 +0100

    capcotor build android and iois

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: 'e92267d053f1b20ba39f57fb76759b4f',
+  baseUrl: 'https://api.themoviedb.org/3',
+  images: 'https://image.tmdb.org/t/p/'
+  
 };
