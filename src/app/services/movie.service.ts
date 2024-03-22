18e9029b08960c90ec302eb2e9af91014602fe17
jonasdevpro <kjonasdevpro@gmail.com>
2024-03-22 17:05:11 +0100

    retouche interface home et detail et traduction api de en vers fr

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -19,10 +19,11 @@ export class MovieService {
   constructor(private http: HttpClient) {}
 
   getTopRateMovies(page: number = 1): Observable<ApiResult> {
-    return this.http.get<ApiResult>(`${environment.baseUrl}/movie/top_rated?api_key=${environment.apiKey}&page=${page}`);
+    return this.http.get<ApiResult>(`${environment.baseUrl}/movie/top_rated?api_key=${environment.apiKey}&page=${page}&language=fr`);
+    // const url = `${environment.baseUrl}/movie/top_rated?api_key=${environment.apiKey}&page=${page}&language=${language}`;
   }
 
   getMovieDetails(id: string): Observable<any> {
-    return this.http.get<any>(`${environment.baseUrl}/movie/${id}?api_key=${environment.apiKey}`);
+    return this.http.get<any>(`${environment.baseUrl}/movie/${id}?api_key=${environment.apiKey}&language=fr`);
   }
 }
